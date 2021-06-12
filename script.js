d4bfce87b9903f4609e5f805fb8c746f339859bb
HCY71 <42717695+HCY71@users.noreply.github.com>
2021-06-13 01:17:25 +0800

    basic frame

--- script.js
+++ script.js
@@ -2,3 +2,44 @@ new fullpage("#fullpage", {
     autoScrolling: true,
     scrollHorizontally: true
 });
+
+
+$(document).ready(function(){
+    
+    var loading = new TimelineMax();
+    loading.fromTo(".upper h2", .6, {
+        autoAlpha: 0,
+        y: 20
+    }, {
+        autoAlpha: 1,
+        y: 0
+    },"+=1")
+    .fromTo(".upper p", .6, {
+        autoAlpha: 0
+        // y: 20
+    }, {
+        autoAlpha: 1
+        // y: 0
+    })
+    .fromTo(".btn1", .6, {
+        autoAlpha: 0,
+        x: -20
+    }, {
+        autoAlpha: 1,
+        x: 0
+    },"-=0.6")
+    .fromTo(".btn2", .6, {
+        autoAlpha: 0,
+        x: 20
+    }, {
+        autoAlpha: 1,
+        x: 0
+    },"-=0.6")
+    .fromTo(".lower .arrow", .6, {
+        autoAlpha: 0,
+        y: 20
+    }, {
+        autoAlpha: 1,
+        y: 0
+    })
+});
